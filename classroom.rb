cb3aeb940d71b3667773d79c31f47f00ac8f90b0
Bushido-brown <orizut@gmail.com>
2022-08-31 09:54:33 -0700

    fix linter errors

--- classroom.rb
+++ classroom.rb
@@ -3,12 +3,12 @@ class Classroom
   attr_reader :students
 
   def initialize(label)
-      @label = label
-      @students = []
-    end
+    @label = label
+    @students = []
+  end
 
   def add_student(student)
-      @students << student
+    @students << student
       student.classroom = self
-    end
+  end
 end
