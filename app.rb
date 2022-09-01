f6603ef378decaee8274ed368c8a2adf082f89f5
Bushido-brown <orizut@gmail.com>
2022-09-01 02:18:03 -0700

    FIX ERRORS WITH MAIN.rb and APP.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require './book'
 require './classroom'
-require './person'
+require './person_class'
 require './rental'
-require './student'
-require './teacher'
+require './student_class'
+require './teacher_class'
 
 class App
   def initialize
